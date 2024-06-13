91681ef0873283080972672c69ec0c368c06df9b
Silas Reinagel <silas.reinagel@gmail.com>
2024-06-13 14:54:33 -0500

    Command Args

--- src/index.js
+++ src/index.js
@@ -5,8 +5,15 @@ import dotenv from 'dotenv';
 import { resilientAlertOnCliff } from './app.js';
 dotenv.config({ path: '.env.local' });
 
+const launchArgs = process.argv.slice(2);
+if (launchArgs.length === 0) {
+    console.error('No launch arguments provided. Exiting...');
+    process.exit(1);
+}
+const configFile = launchArgs[0];
+
 const every15Minutes = '0,15,30,45 * * * *';
-const userConfig = JSON.parse(fs.readFileSync('config-test.json', 'utf8'));
+const userConfig = JSON.parse(fs.readFileSync(configFile, 'utf8'));
 const exec = async () => {
 	await resilientAlertOnCliff(userConfig);
 }
